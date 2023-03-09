12948b29d30268f35fc4228091164c82e9834dfe
Viacheslav Sukhenko <slavichmy@gmail.com>
2023-03-09 11:53:24 +0200

    devise confirmable in development

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :courses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+  devise :database_authenticatable, :registerable,  :trackable,
+         :recoverable, :rememberable, :validatable, :confirmable
 
   def to_s
     email
