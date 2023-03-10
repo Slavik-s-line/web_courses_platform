f114bb880727fbd8a2ae67f7476dc4af87cf49a4
Viacheslav Sukhenko <slavichmy@gmail.com>
2023-03-10 18:52:18 +0200

    gem rolify - edit roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :omniauthable
   devise :database_authenticatable, :registerable,  :trackable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   rolify
   
